ea706344daa8f37a5348647e0c4025eb46bf0c26
micpob <bomboloapp@gmail.com>
2021-12-01 09:35:59 +0100

    Clear notifications on click

--- notifications.js
+++ notifications.js
@@ -4,8 +4,18 @@ const sendNotification = (step, total, limit) => {
       type: 'basic',
       iconUrl: 'Res/Icons/icon48.png',
       title: chrome.i18n.getMessage('notification_title'),
-      message: chrome.i18n.getMessage('notification_message') + limit
+            chrome.notifications.create('LimitReachedNotification', options, () => {
+              chrome.notifications.onClicked.addListener(clearAllNotifications)
+              chrome.notifications.onClosed.addListener(clearAllNotifications)
+            })
+          }
+
+const clearAllNotifications = () => {
+  chrome.notifications.getAll((items) => {
+    if (items) {
+      for (let key in items) {
+        chrome.notifications.clear(key)
     }
-    chrome.notifications.create('LimitReachedNotification', options)          
   }
+  })
 }
