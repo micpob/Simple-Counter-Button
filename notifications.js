d657f39f098b587c652bb2313138ab6893b62a27
micpob <bomboloapp@gmail.com>
2021-12-02 05:44:40 +0100

    Refactor notifications (conditional creation, require interaction)

--- notifications.js
+++ notifications.js
@@ -1,21 +1,36 @@
 const sendNotification = (step, total, limit) => {
-  if (step > 0 && total >= limit || step < 0 && total <= limit) {
-    const options = {
-      type: 'basic',
-      iconUrl: 'Res/Icons/icon48.png',
-      title: chrome.i18n.getMessage('notification_title'),
+  chrome.permissions.contains({permissions: ['notifications']}, (result) => {
+    if (result) {
+      if (step > 0 && total >= limit || step < 0 && total <= limit) {
+        chrome.notifications.getAll((items) => {
+          if (items && Object.keys(items).length > 0) {
+            //console.log('notification already opened:', items)
+          } else {
+            const options = {
+              type: 'basic',
+              iconUrl: 'Res/Icons/icon48.png',
+              title: chrome.i18n.getMessage('notification_title'),
+              message: chrome.i18n.getMessage('notification_message') + limit,
+              requireInteraction: true,
+              priority: 2
+            }
             chrome.notifications.create('LimitReachedNotification', options, () => {
               chrome.notifications.onClicked.addListener(clearAllNotifications)
               chrome.notifications.onClosed.addListener(clearAllNotifications)
             })
           }
+        })
+      }
+    }
+  })
+}
 
 const clearAllNotifications = () => {
   chrome.notifications.getAll((items) => {
     if (items) {
       for (let key in items) {
         chrome.notifications.clear(key)
+      }
     }
-  }
   })
 }
